e3b60234b0c9dede049da8dddbac81c6e0d8f8aa
DeepakM73 <deepak.muddasani73@gmail.com>
2021-09-18 02:58:13 +0530

    Welcome to line comparision

---
+++ linecomparisionoops/LineComparisionOops.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linecomparisionoops;
+
+public class LineComparisionOops {
+
+    public static void main(String[] args) {
+	System.out.print("Welcome to line comparision oops");
+    }
+}
