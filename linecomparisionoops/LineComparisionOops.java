aba7efa96051e46e0fce1d411d90ef68084357c1
DeepakM73 <deepak.muddasani73@gmail.com>
2021-09-21 07:19:26 +0530

    Added: Line Lengths

--- linecomparisionoops/LineComparisionOops.java
+++ linecomparisionoops/LineComparisionOops.java
@@ -1,8 +1,45 @@
 package com.bridgelabz.linecomparisionoops;
+import java.util.Scanner;
+
+/**
+ * Purpose - Program for modeling of line and comparing length of two lines
+ */
 
 public class LineComparisionOops {
 
+    /**
+     * Purpose - Modeling of line
+     *
+     * @param lineNum coordinate of lines
+     * @return length of line
+     */
+    public static double lineLength(int lineNum) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x1 point");
+        int x1 = sc.nextInt();
+        System.out.println("Enter y1 point");
+        int y1 = sc.nextInt();
+        System.out.println("Enter x2 point");
+        int x2 = sc.nextInt();
+        System.out.println("Enter y2 point");
+        int y2 = sc.nextInt();
+
+        int X = x2 - x1;
+        int Y = y2 - y1;
+        double length = Math.sqrt((X * X) + (Y * Y));
+        System.out.println("Length of Line-" + lineNum + " = " + length + "unit");
+
+        return length;
+    }
+
+    /**
+     * Purpose - Give final output
+     */
     public static void main(String[] args) {
-	System.out.print("Welcome to line comparision oops");
+        System.out.println("Welcome to Line Comparison Program");
+
+        System.out.println("Enter coordinate of line-1");
+        lineLength(1);
     }
 }
